df1e8dcf99ad039c69195c9f0b918b065fe3905e
innovative123 <94621080+innovative123@users.noreply.github.com>
2023-12-26 09:52:45 +0530

    Assignment 1 of javascript
    Assignment 1 of javascript

---
+++ 01_variableAssign.js
@@ -0,0 +1,29 @@
+var myFullName;
+console.log("FullName :",myFullName);
+myFullName = "Kuldeep Sonawane";
+console.log("FullName :",myFullName);
+myFullName = "Kuldeep Dinesh Sonawane";
+console.log("FullName :",myFullName);
+
+
+console.log("");
+var mylove = "I love Only Js";
+console.log(mylove); 
+
+console.log("");
+var firstName = "Kuldeep";
+var lastName = "Sonawane";
+var Age = "25";
+var address = "AAbbcc";
+var pinCode = "424001";
+var gender = "Male";
+var isMarried = "No";
+
+console.log("FirstName :",firstName);
+console.log("lastName :",lastName);
+console.log("Age :",Age);
+console.log("Address :",address);
+console.log("Pincode :",pinCode);
+console.log("Gender :",gender);
+console.log("isMarried :",isMarried);
+
