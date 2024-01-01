bca92f597bf373a2858611d7ecdf3cc65b40db65
innovative123 <94621080+innovative123@users.noreply.github.com>
2024-01-01 14:52:59 +0530

    qperaterupdate
    operateupdate

---
+++ 04_operators.js
@@ -0,0 +1,68 @@
+
+
+
+
+
+
+function SquareOftheWordLength(){
+
+    var a = "Javascript";
+    var b = a.length;
+    var c = b * b;
+    console.log("-----------------------------------Step 1----------------------------------------");
+    console.log(`The Length of the String "Javascript" is: ${c}`);
+
+    var d = "Google Chrome";
+    var e = d.length;
+    var f = d.length * d.length;
+
+    console.log(`The Length of the String "Google Chrome" is: ${f}`);
+
+    var g = "Developer Smart";
+    var h = g.length;
+    var i = g.length * g.length;
+
+    console.log(`The Length of the String "Developer Smart" is: ${i}`);
+
+    console.log("");
+
+
+    
+
+
+}
+
+SquareOftheWordLength();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function javascript(){
+    var a = "I am Angular Developer";
+    var b = a.length;
+    var c = a.split(" ");
+    var d = c.length;
+    var sum = b / d
+    var Mul = b * d;
+    console.log("-----------------------------------Step 2----------------------------------------");
+    console.log("Length of the String is :",a.length);
+    console.log("Total no of words in the String is:",d);
+    console.log("Division of the Length of String by total number of words in the String:",sum);
+    console.log("Multiplication of the Length of String by total number of words in the string:",Mul);
+
+}
+javascript()
